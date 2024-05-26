8ee9da717f578216b7a452e843fe77c69c9e1688
Mohamed El attar <mohamadelattar99@gmail.com>
2024-05-26 18:49:53 +0100

    "Two Sum problem"

---
+++ Two_Sum/Solution.java
@@ -0,0 +1,17 @@
+package Two_Sum;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(tracker.containsKey(nums[i])){
+                int left = tracker.get(nums[i]);
+                return new int[]{left, i};
+            }else
+                tracker.put(target - nums[i], i);
+        }
+        return new int[2];
+    }
+}
