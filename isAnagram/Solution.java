f0d3126f14e0f87eaf3898b3d358524b14614fe8
Mohamed El attar <mohamadelattar99@gmail.com>
2024-11-09 18:01:46 +0100

    "isAnagram"

---
+++ isAnagram/Solution.java
@@ -0,0 +1,16 @@
+package isAnagram;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+}
