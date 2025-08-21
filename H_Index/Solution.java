4163ffc480ecad6a612ca0dd180f6b7cb70b5b7f
Mohamed El attar <mohamadelattar99@gmail.com>
2025-08-21 21:32:21 +0100

    "H-index"

---
+++ H_Index/Solution.java
@@ -0,0 +1,25 @@
+package H_Index;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(hIndex(new int[]{3,0,6,1,5}));
+    }
+    public static int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
+            }
+        }
+
+        return 0;
+    }
+}
